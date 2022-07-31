e1cf37bdab73590be877decb9ab6409d3c051b01
arviyonismara <arviyonismara@gmail.com>
2022-07-31 20:14:06 +0700

    tambah

---
+++ LatihanWaktuSederhana.py
@@ -0,0 +1,8 @@
+﻿hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+time_hour = (hour + dura//60 + (mins+ dura%60)//60)%24
+time_min = (mins+ dura%60)%60
+
+print(time_hour,time_min,sep=":")
