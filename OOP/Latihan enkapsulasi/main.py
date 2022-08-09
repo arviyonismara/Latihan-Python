9b0ee5a9c09ef7d63b8a2464f66c20e2e2c72ec1
arviyonismara <arviyonismara@gmail.com>
2022-08-09 17:52:16 +0700

    latihan en

--- OOP/Latihan enkapsulasi/main.py
+++ OOP/Latihan enkapsulasi/main.py
@@ -1,45 +1,26 @@
-# latihan enkapsulasi
 class Hero:
 
-	def __init__(self, name, health, armour):
-		self.name = name
-		self.__health = health
-		self.__armour = armour
-		# self.__info = "name {} : \n\thealth: {}".format(self.name,self.__health)
+	__jumlah = 0
+
+	def __init__(self, name, health, armour, power):
+		self.__name = name
+		self.__healthStandar = health
+		self.__armourStandar = armour
+		self.__powerStandar = power
+		self.__level = 1
+		self.exp = 0
+
+		self.__healthMax = self.__healthStandar * self.__level
+		self.__power = self.__powerStandar * self.__level
+		self.__armour = self.__armourStandar * self.__level
+
+		self.__health = self.__healthMax
+
+		Hero.__jumlah += 1
 
 	@property
 	def info(self):
-		return "name {} : \n\thealth: {}".format(self.name,self.__health)
-		# return self.__info
+		return "name {} : \n\thealth : {}/{}".format(self.__name, self.__health,self.__healthMax)
 
-	@property
-	def armour(self):
-		pass
-
-	@armour.getter
-	def armour(self):
-		return self.__armour
-
-	@armour.setter
-	def armour(self, input):
-		self.__armour = input
-
-	@armour.deleter
-	def armour(self):
-		print("armour didelete")
-		self.__armour = None
-
-sniper = Hero("Sniper", 100, 10)
-# print(sniper.getHealth())
-print(sniper.info)
-sniper.name = "Dadang"
-print(sniper.info)
-
-print("getter dan setter untuk __armour")
-print(sniper.armour)
-sniper.armour = 50
-print(sniper.armour)
-
-print("delete armour")
-del sniper.armour
-print(sniper.__dict__)
+Balmond = Hero("Balmond", 400, 20, 10)
+print(Balmond.info)
